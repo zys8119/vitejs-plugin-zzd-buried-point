b0d8122232aec523c8a05c1199866be408e2049a
zys8119 <770959294@qq.com>
2022-04-19 09:02:26 +0800

    [add] 浙政钉埋点

--- index.ts
+++ index.ts
@@ -32,7 +32,7 @@ export default (config:zzdCodeConfig) => {
             }
             return mergeConfig(config,<UserConfig>{
                 define:{
-                    zzdCode:VITE_ZZD_CODE ? `'${VITE_ZZD_CODE}'` : null
+                    zzdCode:VITE_ZZD_CODE ? `'${VITE_ZZD_CODE}'` : `'null'`
                 },
             })
             //todo  浙政钉埋点 code 禁止删除==================
